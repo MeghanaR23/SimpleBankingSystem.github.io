37314bc3898ebc8c6b885caa2f293bfc3d81cb24
MeghanaR23 <66193827+MeghanaR23@users.noreply.github.com>
2021-07-12 17:45:58 +0530

    Initial Commit
    Contains images and css for the banking system project

---
+++ index.php
@@ -0,0 +1,58 @@
+<html>
+
+<head>
+    <link rel="stylesheet" href="css/button1.css">
+    <link rel="stylesheet" href="css/button2.css">
+
+
+    <title> Simple Banking System</title>
+    <style>
+        button {
+            background-color: #D3D3D3;
+        }
+
+        tr {
+            text-align: center;
+        }
+
+        body {
+            text-align: center;
+        }
+
+        body {
+            background-image: url("images/3.jpg");
+            background-position: center;
+            background-size: cover;
+        }
+    </style>
+
+</head>
+
+<body>
+    <br>
+    <br>
+    <br>
+    <h2 style=" font-family:Times new roman; font-size: 55px;color:#ff6347;text-shadow: 2px 2px black;"> Simple Banking System </h2>
+    </div>
+    <div id="section">
+        <table>
+            <tr></tr>
+            <tr><br>
+                <a href="getdetails.php">
+                    <button class="btn" type="button"><span>View Users</span></button>
+                </a>
+            </tr>
+
+            <tr>
+                <br> <br> <br>
+                <a href="transactions.php">
+                    <button class="btn" type="button"><span>All Transactions</span></button>
+                </a>
+
+            </tr>
+
+        </table>
+    </div>
+</body>
+
+</html>
---
+++ readme.txt
@@ -0,0 +1,2 @@
+#Simple Banking System
+The Simple Banking System is where users can transfer money with multiple users.
